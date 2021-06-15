45974693912a1a0eb90c5c20be34370efbf05695
frank <franktranvantu@gmail.com>
2021-06-15 18:27:31 +0700

    Update

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -2,7 +2,7 @@ const logger = store => next => action => {
   console.log('Store', store);
   console.log('Next', next);
   console.log('Action', action);
-  // next(action);
+  next(action);
 }
 
 export default logger;
