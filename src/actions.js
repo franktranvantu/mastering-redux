9205e67a1d031e9ef2c116d1402bd612f6ef4891
frank <franktranvantu@gmail.com>
2021-06-15 12:58:06 +0700

    Update

--- src/actions.js
+++ src/actions.js
@@ -3,6 +3,13 @@ import * as actions from "./actionTypes";
 export const bugAdded = description => ({
   type: actions.BUG_ADDED,
   payload: {
-    description: description
+    description
+  }
+});
+
+export const bugRemoved = id => ({
+  type: actions.BUG_REMOVED,
+  payload: {
+    id
   }
 });
--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import store from "./store";
-import * as actions from "./actionTypes";
-import {bugAdded} from "./actions";
-
-const unsubscribe = store.subscribe(() => {
-  console.log('Store changed!', store.getState());
-});
+import {bugAdded, bugRemoved} from "./actions";
 
 store.dispatch(bugAdded('Bug 1'));
 
-unsubscribe();
-
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1
-  }
-})
+store.dispatch(bugRemoved(1));
 
 console.log(store.getState());
